488ee4aea1493a32417a47eb397d02d1c24e3473
kyarkova <ksenia.yarkova@gmail.com>
2017-10-23 14:39:24 +0300

    correct db name in files to library

--- src/main/java/org/library/db/dao/DAOFactory.java
+++ src/main/java/org/library/db/dao/DAOFactory.java
@@ -24,7 +24,7 @@ public class DAOFactory
     {
         if( factory == null )
         {
-            factory = Persistence.createEntityManagerFactory( "doctor" );
+            factory = Persistence.createEntityManagerFactory( "library" );
             int i = 0;
         }
         return _instance;
