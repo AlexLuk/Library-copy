4f3cbab240a58044eb8b5f11eb505287a0d92f7b
loryrosh <sasharo@mail.ru>
2017-10-27 18:04:25 +0300

    Spring test - hello world
    open http://localhost:8080/

--- src/main/java/org/library/services/LibraryController.java
+++ src/main/java/org/library/services/LibraryController.java
@@ -7,13 +7,8 @@ import org.springframework.web.bind.annotation.*;
 public class LibraryController {
     public LibraryController() {}
 
-    @RequestMapping
-    (
-        value = "/",
-        produces = { MediaType.APPLICATION_JSON_VALUE },
-        method = RequestMethod.GET
-    )
-    public ResponseEntity<?> echo () {
-        return new ResponseEntity<String>( "Hello World!", HttpStatus.OK );
+    @RequestMapping("/")
+    public String echo () {
+        return "Hello World!";
     }
 }
