fdfb3b7eb50c1d73983767243b2c9d98d3bddff9
loryrosh <sasharo@mail.ru>
2017-10-27 18:19:02 +0300

    Spring test - Сестра Керри!

--- src/main/java/org/library/services/LibraryController.java
+++ src/main/java/org/library/services/LibraryController.java
@@ -1,7 +1,13 @@
 package org.library.services;
 
+import org.library.db.dao.DAOFactory;
+import org.library.db.dao.impl.BookDAO;
+import org.library.db.models.Book;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @RestController
 public class LibraryController {
@@ -9,6 +15,26 @@ public class LibraryController {
 
     @RequestMapping("/")
     public String echo () {
-        return "Hello World!";
+        DAOFactory daoFactory = DAOFactory.getInstance();
+
+        String[] res = new String[3];
+        if (daoFactory.isConnected()) {
+            Optional<BookDAO> dao = daoFactory.getModel(BookDAO.class);
+            dao.ifPresent(daoObj -> {
+                daoObj.getByTitle("Керри").ifPresent(obj -> {
+                    for(Book book : obj) {
+                        System.out.println(book.getTitle());
+                        res[0] = book.getTitle();
+                        System.out.println(book.getGenre().getName());
+                        res[1] = book.getGenre().getName();
+                        System.out.println(book.getIsRare());
+                        res[2] = "" + book.getIsRare();
+                    }
+                });
+            });
+        }
+        daoFactory.closeConn();
+
+        return Arrays.toString(res);
     }
 }
