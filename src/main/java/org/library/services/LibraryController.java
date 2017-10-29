0d7f4c4502290fed0bf92d0170fd8af6471600b0
loryrosh <sasharo@mail.ru>
2017-10-29 19:49:51 +0300

    add jsp config

--- src/main/java/org/library/services/LibraryController.java
+++ src/main/java/org/library/services/LibraryController.java
@@ -3,29 +3,35 @@ package org.library.services;
 import org.library.db.dao.DAOFactory;
 import org.library.db.dao.impl.BookDAO;
 import org.library.db.models.Book;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class LibraryController {
     DAOFactory _daoFactory = DAOFactory.getInstance();
 
     public LibraryController() {}
 
     @RequestMapping("/")
-    public String echo () {
+    public String home(Map<String, Object> model) {
+        //model.put("message", "Hello");
+        return "index";
+    }
+
+    @RequestMapping("/search")
+    public String auth() {
         String[] res = new String[1];
         if (_daoFactory.isConnected()) {
             Optional<BookDAO> dao = _daoFactory.getModel(BookDAO.class);
-            dao.ifPresent(daoObj -> {
-                daoObj.getByTitle("Керри").ifPresent(obj -> {
-                    for(Book book : obj) {
-                        res[0] = book.getTitle() + " - " + book.getGenre().getName() + " - " + book.getIsRare();
-                    }
-                });
-            });
+            dao.ifPresent(daoObj -> daoObj.getByTitle("Керри").ifPresent(obj -> {
+                for(Book book : obj) {
+                    res[0] = book.getTitle() + " - " + book.getGenre().getName() + " - " + book.getIsRare();
+                }
+            }));
         }
         return Arrays.toString(res);
     }
