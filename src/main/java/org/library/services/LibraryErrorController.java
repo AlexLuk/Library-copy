e30e78645877ce8a6adf22a3729a18364ce4ad44
kyarkova <ksenia.yarkova@gmail.com>
2017-10-31 11:15:21 +0300

    add error controller

---
+++ src/main/java/org/library/services/LibraryErrorController.java
@@ -0,0 +1,28 @@
+package org.library.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LibraryErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error(HttpServletResponse response) {
+        System.out.println(response.getStatus());
+        if (response.getStatus() == 404) {
+            return "/errorPages/error404";
+        } else return "/errorPages/error500";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
