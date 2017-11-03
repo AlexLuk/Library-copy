d4563fbab7fca4cb957369972b6a554c79a20a36
Alexandr <alexlukovkin@gmail.com>
2017-11-03 17:58:38 +0300

    Wrote method isPasswordComplicate for testing password from user.  Added tests for it.  Shortened isEmailUnique

--- src/main/java/org/library/controllers/LibraryRegistrationController.java
+++ src/main/java/org/library/controllers/LibraryRegistrationController.java
@@ -8,10 +8,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class LibraryRegistrationController {
+
+    public static final String passwordREGEXP = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!?.,/@#$%^&+=])(?=\\S+$).{8,}$";
+//    ^                 # start-of-string
+//            (?=.*[0-9])       # a digit must occur at least once
+//            (?=.*[a-z])       # a lower case letter must occur at least once
+//            (?=.*[A-Z])       # an upper case letter must occur at least once
+//            (?=.*[@#$%^&+=])  # a special character must occur at least once
+//            (?=\S+$)          # no whitespace allowed in the entire string
+//            .{8,}             # anything, at least eight places though
+//    $                 # end-of-string
+
     public LibraryRegistrationController() {
     }
 
@@ -19,20 +28,23 @@ public class LibraryRegistrationController {
     ReaderRepository readerRepository;
 
     @RequestMapping(value = {"/checks/email"}, method = RequestMethod.POST)
-    public boolean isEmailUnique(@ModelAttribute Reader reader) {
-        if (readerRepository.findByEmail(reader.getEmail().toLowerCase()).isPresent()) {
-            return false;
-        } else {
-            return true;
-        }
+    public boolean isEmailUnique(String email) {
+        return !readerRepository.findByEmail(email.toLowerCase()).isPresent();
     }
 
-    @RequestMapping(value = {"/checks/email"}, method = RequestMethod.POST)
-    public boolean isPasswordCommplicate(String password) {
-        return false;
+    @RequestMapping(value = {"/checks/password"}, method = RequestMethod.POST)
+    public boolean isPasswordComplicate(String password, String email) {
+        if (email.length() == 0) return false;
+        String userLogin;
+        if (email.contains("@")){
+            userLogin = email.substring(0, email.indexOf("@"));
+        }else return false;
+        if (userLogin.length() == 0) return false;
+        return password.matches(passwordREGEXP) && !password.matches(".*?" + userLogin + ".*?");
     }
 
-//    @RequestMapping(value = {"/registration"}, method = RequestMethod.POST)
+
+    //    @RequestMapping(value = {"/registration"}, method = RequestMethod.POST)
     @RequestMapping(value = {"/"}, method = RequestMethod.POST)
     public String registerUser(@ModelAttribute Reader reader) {
         reader.setEmail(reader.getEmail().toLowerCase());
