23352d7f389d9b33076e6ba45274ca092b02d19b
Alexandr <alexlukovkin@gmail.com>
2017-11-03 18:11:01 +0300

    Added Javadoc Added not case sensitive check for login in password

--- src/test/java/org/library/controllers/LibraryRegistrationControllerTest.java
+++ src/test/java/org/library/controllers/LibraryRegistrationControllerTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class LibraryRegistrationControllerTest {
-    LibraryRegistrationController libraryRegistrationController;
+    private LibraryRegistrationController libraryRegistrationController;
 
     @Before
     public void setTests() {
