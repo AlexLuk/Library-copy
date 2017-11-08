9b4a6ff08d6fd408b6da7229642677798cd60732
Alexandr <alexlukovkin@gmail.com>
2017-11-09 01:49:37 +0300

    Add test scope for order service

--- src/test/java/org.library/services/OrderServiceTest.java
+++ src/test/java/org.library/services/OrderServiceTest.java
@@ -1,17 +1,27 @@
 package org.library.services;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.*;
 
 public class OrderServiceTest extends LibraryTest{
+
+    @Autowired
+    OrderService orderService;
+
     @Test
-    public void getByReaderByStatus() throws Exception {
+    public void getByReaderAndBook() throws Exception {
 
     }
 
     @Test
-    public void getAllOrdersByReader() throws Exception {
+    public void getByReaderIdAndBookItemIdIn() throws Exception {
+
+    }
+
+    @Test
+    public void findByBookId() throws Exception {
 
     }
 
@@ -19,4 +29,5 @@ public class OrderServiceTest extends LibraryTest{
     public void addOrder() throws Exception {
 
     }
+
 }
