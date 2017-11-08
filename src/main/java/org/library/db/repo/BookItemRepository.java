c672967944be191fcd4774e527090ced255fc86d
Bz-zik <narciss95@yandex.ru>
2017-11-09 00:51:45 +0300

    Service and Controller for Delivery (for button in admin's account)

--- src/main/java/org/library/db/repo/BookItemRepository.java
+++ src/main/java/org/library/db/repo/BookItemRepository.java
@@ -3,6 +3,8 @@ package org.library.db.repo;
 import org.library.db.domain.BookItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookItemRepository extends JpaRepository<BookItem, Integer> {
+import java.util.List;
 
+public interface BookItemRepository extends JpaRepository<BookItem, Integer> {
+    List<BookItem> findByBookId(int bookId);
 }
--- src/main/java/org/library/db/repo/ItemStatusRepository.java
+++ src/main/java/org/library/db/repo/ItemStatusRepository.java
@@ -3,6 +3,8 @@ package org.library.db.repo;
 import org.library.db.domain.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemStatusRepository extends JpaRepository<ItemStatus, Integer> {
+import java.util.List;
 
+public interface ItemStatusRepository extends JpaRepository<ItemStatus, Integer> {
+    List<ItemStatus> findByName(String name);
 }
