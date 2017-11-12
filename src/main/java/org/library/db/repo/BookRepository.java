1b12262f1ff41ca2d8d97ac4659a5124122c7e24
Alexandr <alexlukovkin@gmail.com>
2017-11-12 14:37:51 +0300

    Fix bug with not one books entry output for muliauthor book Add book order by author, then by title

--- src/main/java/org/library/db/repo/BookRepository.java
+++ src/main/java/org/library/db/repo/BookRepository.java
@@ -12,13 +12,13 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
-    @Query("select book from Book book " +
+    @Query("select distinct book from Book book " +
             "inner join book.authors bookAuthor" +
             " where lower(book.title) like lower(concat('%',:title,'%')) " +
             " and (book.year = :year or :year is null)" +
             " and (book.genre.id =:genreId or :genreId is null)" +
             " and bookAuthor in(select author from Author author where lower(author.lastName) like lower(concat('%',:lastName,'%')))" +
-            "order by bookAuthor.lastName")
+            "order by bookAuthor.lastName, book.title")
     List<Book> finaByComplexQuery(@Param("title") String title, @Param("year") Integer year,
                                   @Param("genreId") Integer genreId, @Param("lastName") String lastName);
 }
