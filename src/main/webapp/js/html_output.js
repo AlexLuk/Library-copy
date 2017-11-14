9bbfad829cb35807bb9b58033189252b056726ca
loryrosh <sasharo@mail.ru>
2017-11-15 02:28:51 +0300

    filter with disabled buttons and rare flag

--- src/main/webapp/js/html_output.js
+++ src/main/webapp/js/html_output.js
@@ -11,8 +11,6 @@ function filterOutput(resp) {
         htmlContent.append($('<td>' + data.authors + '</td>'));
         htmlContent.append($('<td>' + data.year + '</td>'));
         htmlContent.append($('<td>' + data.genre + '</td>'));
-        htmlContent.append($('<td>' + data.isRare + '</td>'));
-        htmlContent.append($('<td>' + data.amount + '</td>'));
 
         var td = $('<td></td>');
         if (data.isRare) {
