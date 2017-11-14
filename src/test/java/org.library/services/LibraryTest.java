caad5288a26903a4f0ab4902d83adacea6193f71
Alexandr <alexlukovkin@gmail.com>
2017-11-14 13:55:52 +0300

    Codestyle fix

--- src/test/java/org.library/services/LibraryTest.java
+++ src/test/java/org.library/services/LibraryTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -62,7 +61,6 @@ public class LibraryTest {
         bookItemsSetup();
         deliveriesSetup();
     }
-    //todo use array list
 
     private void genresSetup() {
         testGenres = new ArrayList<>();
@@ -95,15 +93,15 @@ public class LibraryTest {
 
     private void readersSetup() {
         testReaders = new ArrayList<>();
-        testReaders.add(new Reader("amma@mail.ru","pwd","am","ma","",new Date(System.currentTimeMillis()),0.0,false));
-        testReaders.add(new Reader("maam@mail.ru","pwd","ma","am","",new Date(System.currentTimeMillis()),0.0,false));
+        testReaders.add(new Reader("amma@mail.ru", "pwd", "am", "ma", "", new Date(System.currentTimeMillis()), 0.0, false));
+        testReaders.add(new Reader("maam@mail.ru", "pwd", "ma", "am", "", new Date(System.currentTimeMillis()), 0.0, false));
     }
 
     private void bookOrdersSetup() {
         testBookOrders = new ArrayList<>();
-        testBookOrders.add(new BookOrder(testReaders.get(0),testBooks.get(0),true));
-        testBookOrders.add(new BookOrder(testReaders.get(0),testBooks.get(1),true));
-        testBookOrders.add(new BookOrder(testReaders.get(1),testBooks.get(2),true));
+        testBookOrders.add(new BookOrder(testReaders.get(0), testBooks.get(0), true));
+        testBookOrders.add(new BookOrder(testReaders.get(0), testBooks.get(1), true));
+        testBookOrders.add(new BookOrder(testReaders.get(1), testBooks.get(2), true));
     }
 
     private void itemStatusesSetup() {
@@ -115,23 +113,23 @@ public class LibraryTest {
 
     private void bookItemsSetup() {
         testBookItems = new ArrayList<>();
-        testBookItems.add(new BookItem(1,testBooks.get(0),testItemStatuses.get(1)));
-        testBookItems.add(new BookItem(2,testBooks.get(1),testItemStatuses.get(1)));
-        testBookItems.add(new BookItem(3,testBooks.get(2),testItemStatuses.get(1)));
-        testBookItems.add(new BookItem(4,testBooks.get(0),testItemStatuses.get(1)));
-        testBookItems.add(new BookItem(5,testBooks.get(0),testItemStatuses.get(0)));
-        testBookItems.add(new BookItem(6,testBooks.get(0),testItemStatuses.get(0)));
-        testBookItems.add(new BookItem(7,testBooks.get(1),testItemStatuses.get(0)));
-        testBookItems.add(new BookItem(8,testBooks.get(1),testItemStatuses.get(0)));
-        testBookItems.add(new BookItem(9,testBooks.get(2),testItemStatuses.get(0)));
+        testBookItems.add(new BookItem(1, testBooks.get(0), testItemStatuses.get(1)));
+        testBookItems.add(new BookItem(2, testBooks.get(1), testItemStatuses.get(1)));
+        testBookItems.add(new BookItem(3, testBooks.get(2), testItemStatuses.get(1)));
+        testBookItems.add(new BookItem(4, testBooks.get(0), testItemStatuses.get(1)));
+        testBookItems.add(new BookItem(5, testBooks.get(0), testItemStatuses.get(0)));
+        testBookItems.add(new BookItem(6, testBooks.get(0), testItemStatuses.get(0)));
+        testBookItems.add(new BookItem(7, testBooks.get(1), testItemStatuses.get(0)));
+        testBookItems.add(new BookItem(8, testBooks.get(1), testItemStatuses.get(0)));
+        testBookItems.add(new BookItem(9, testBooks.get(2), testItemStatuses.get(0)));
     }
 
     private void deliveriesSetup() {
         testDeliveries = new ArrayList<>();
-        testDeliveries.add(new Delivery(testReaders.get(0),testBookItems.get(0)));
-        testDeliveries.add(new Delivery(testReaders.get(0),testBookItems.get(1)));
-        testDeliveries.add(new Delivery(testReaders.get(0),testBookItems.get(2)));
-        testDeliveries.add(new Delivery(testReaders.get(1),testBookItems.get(3)));
+        testDeliveries.add(new Delivery(testReaders.get(0), testBookItems.get(0)));
+        testDeliveries.add(new Delivery(testReaders.get(0), testBookItems.get(1)));
+        testDeliveries.add(new Delivery(testReaders.get(0), testBookItems.get(2)));
+        testDeliveries.add(new Delivery(testReaders.get(1), testBookItems.get(3)));
     }
 
 
